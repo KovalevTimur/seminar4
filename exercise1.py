2af53d94ef7d13b43792dc18986ff90c70a458eb
KovalevTimur <mfallendown@gmail.com>
2021-10-23 12:19:44 +0500

    domasha1

--- exercise1.py
+++ exercise1.py
@@ -1,7 +1,7 @@
 a = float(input('Введите первое число: '))
 b = float(input('Введите второе число: '))
 def exe1(num1, num2):
-       if abs(num1-num2) == 100: 
+       if abs(num1-num2) >= 100: 
              print('Yes')
        else: 
 
