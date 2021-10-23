6a454185270239171e6a7de9dc0dec1d859c7ee0
KovalevTimur <mfallendown@gmail.com>
2021-10-23 11:28:52 +0500

    zadacha1

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+a = float(input('Введите первое число: '))
+b = float(input('Введите второе число: '))
+def exe1(num1, num2):
+       if abs(num1-num2) == 100: 
+             print('Yes')
+       else: 
+
+             print('No')
+exe1(a,b) 
+
+
